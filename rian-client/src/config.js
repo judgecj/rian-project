e8e679b617559734cc1584a45f021e13985c914a
gojeje <hello.gojeje@gmail.com>
2021-05-04 11:54:41 +0100

    update we 1

--- rian-client/src/config.js
+++ rian-client/src/config.js
@@ -1,5 +1,4 @@
 exports.PORT = process.env.PORT || 8080
 
-exports.CLIENT_ORIGIN = process.env.NODE_ENV === 'production' 
-  ? process.env.CLIENT_ORIGIN
-  : 'https://rian-appnew.herokuapp.com'
+exports.CLIENT_ORIGIN  = 'https://rian-appnew.herokuapp.com' 
+ 
